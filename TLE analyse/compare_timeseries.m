80ea3da80410d4f0228f7a34da87f17597ae4758
ziqing97 <ziqing.yu0623@gmail.com>
2022-11-26 00:33:55 +0100

    Update compare_timeseries.m

--- TLE analyse/compare_timeseries.m
+++ TLE analyse/compare_timeseries.m
@@ -1,34 +1,50 @@
-function [] = compare_timeseries(data,name,time_split)
-xmin = data.time(1) - 60;
-xmax = data.time(end) + 60;
+function [] = compare_timeseries(data_list,name_list)
+% 
 
-time = [data.time(1),time_split,data.time(end)];
-legende = cell(length(time)-1,1);
+% get the x limit
+xmax = 0;
+xmin = 1e50;
+for j=1:length(data_list)
+    if  data_list{j}.time(1)<xmin
+        xmin = data_list{j}.time(1);
+    end
+    if data_list{j}.time(end)>xmax
+        xmax = data_list{j}.time(end);
+    end
+end
+xmin = xmin - 60;
+xmax = xmax + 60;
+
+% get the title
+name ='';
+for i=1:length(name_list)
+    name = [name,',',name_list{i}];
+end
+name = name(2:end);
+
 % well, just do some plotting things
 fig1 = figure;
+
+% e
 subplot(3,1,1)
 hold on
-for i=1:length(time)-1
-    id = data.time>=time(i) & data.time<=time(i+1);
-    plot(data.time(id),data.e(id))
-    legende{i} = [datestr(datetime(round(time(i)),'ConvertFrom','datenum')),...
-        ' --- ',datestr(datetime(round(time(i+1)),'ConvertFrom','datenum'))];
+for j=1:length(data_list)
+    plot(data_list{j}.time,data_list{j}.e)
 end
 title([name,' eccentricity'])
-%xlabel('Time')
 ylabel('Eccentricity','FontSize',2)
-legend(legende)
+legend(name_list)
 set(gca,'FontSize',11)
 datetick('x')
 grid on
 yticks(0:90:360)
 xlim([xmin,xmax])
 
+% height
 subplot(3,1,3)
 hold on
-for i=1:length(time)-1
-    id = data.time>=time(i) & data.time<=time(i+1);
-    plot(data.time(id),data.a(id)/1000-6371)
+for j=1:length(data_list)
+    plot(data_list{j}.time,data_list{j}.a/1000-6371)
 end
 title([name,' height'])
 xlabel('Time')
@@ -39,15 +55,14 @@ grid on
 yticks(0:45:360)
 xlim([xmin,xmax])
 
+% AoP
 subplot(3,1,2)
 hold on
-for i=1:length(time)-1
-    id = data.time>=time(i) & data.time<=time(i+1);
-    plot(data.time(id),data.AoP(id))
+for j=1:length(data_list)
+    plot(data_list{j}.time,data_list{j}.AoP)
 end
-plot(data.time,ones(length(data.time),1)*90,"LineWidth",2,'color','k')
+plot([xmin,xmax],ones(2,1)*90,"LineWidth",2,'color','k') % plot the 90 degree line
 title([name,' Argument of Perigee'])
-%xlabel('Time')
 ylabel('Argument of Perigee [°]')
 yticks([0 90 180 270 360])
 set(gca,'FontSize',11)
@@ -56,6 +71,7 @@ grid on
 yticks(0:45:360)
 xlim([xmin,xmax])
 
+% save the plot
 set(gcf,'units','normalized','outerposition',[0 0 1 1])
 saveas(fig1,['element_',name],"png")
 end
