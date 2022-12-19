a14bd6a40d188f7467dd94a6091680f57beea31e
ziqing97 <ziqing.yu0623@gmail.com>
2022-12-19 20:11:24 +0100

    ppt Ziqing YU

--- Project_Space_Debris/Ordner_Yu/main.m
+++ Project_Space_Debris/Ordner_Yu/main.m
@@ -14,3 +14,9 @@ title('Space Objects Count in Low Earth Orbit')
 set(gca,'FontSize',24)
 set(gca,'yticklabel',get(gca,'ytick'));
 
+data_count = satellitestatistic{:,2};
+d_c = diff(data_count);
+figure
+plot(satellitestatistic{2:end,1},d_c)
+grid on
+xticks(1960:5:2025)
