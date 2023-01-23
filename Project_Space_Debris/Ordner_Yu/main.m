d2d7e23f0ba2b136a4da07847aed671a792ac83b
ZIQING YU <ziqing.yu0623@gmail.com>
2023-01-23 15:59:06 +0100

    update

--- Project_Space_Debris/Ordner_Yu/main.m
+++ Project_Space_Debris/Ordner_Yu/main.m
@@ -20,41 +20,55 @@ GM = 3.9865005e14;   % m^3/s^2
 T = sqrt((a^3 * 4* pi) / (GM)); % s
 
 %%
+% 35 seconds for one day for harris-priester model
 starttime = datetime(2022,12,1,0,0,0,'TimeZone','UTC');
-tspan = 0:24*3600:30*24*3600; % integral time
+tspan = 0:24*3600:24*3600; % integral time
 
 for i=1:length(tspan)-1
     tspan_split=[tspan(i),tspan(i+1)];
     t0 = starttime+tspan(i)/3600/24;
     if i==1
-        result_noforce = orbit_integral(a,e,I,Omega,...
-            w,M,AdM,t0,tspan_split,"noforce");
-        result_harris_priester = orbit_integral(a,e,I,Omega,...
-            w,M,AdM,t0,tspan_split,"hp");
+%         result_noforce = orbit_integral(a,e,I,Omega,...
+%             w,M,AdM,t0,tspan_split,"noforce");
+%         result_harris_priester = orbit_integral(a,e,I,Omega,...
+%             w,M,AdM,t0,tspan_split,"hp");
+        result_msis = orbit_integral(a,e,I,Omega,...
+            w,M,AdM,t0,tspan_split,"msis");
     else
-        % noforce
-        a = result_noforce(end,2);              % meter
-        e = result_noforce(end,3);      % radiant
-        I = result_noforce(end,4);
-        Omega = result_noforce(end,5);   % radiant
-        w = result_noforce(end,6);       % radiant
-        M = result_noforce(end,7);       % radiant
-        result_noforce = orbit_integral(a,e,I,Omega,...
-            w,M,AdM,t0,tspan_split,"noforce");
+%         % noforce
+%         a = result_noforce(end,2);              % meter
+%         e = result_noforce(end,3);      % radiant
+%         I = result_noforce(end,4);
+%         Omega = result_noforce(end,5);   % radiant
+%         w = result_noforce(end,6);       % radiant
+%         M = result_noforce(end,7);       % radiant
+%         result_noforce = orbit_integral(a,e,I,Omega,...
+%             w,M,AdM,t0,tspan_split,"noforce");
+%         % harris-priester model
+%         a = result_harris_priester(end,2);      % meter
+%         e = result_harris_priester(end,3);      % radiant
+%         I = result_harris_priester(end,4);
+%         Omega = result_harris_priester(end,5);   % radiant
+%         w = result_harris_priester(end,6);       % radiant
+%         M = result_harris_priester(end,7);       % radiant
+%         result_harris_priester = orbit_integral(a,e,I,Omega,...
+%             w,M,AdM,t0,tspan_split,"hp");
         % harris-priester model
-        a = result_harris_priester(end,2);      % meter
-        e = result_harris_priester(end,3);      % radiant
-        I = result_harris_priester(end,4);
-        Omega = result_harris_priester(end,5);   % radiant
-        w = result_harris_priester(end,6);       % radiant
-        M = result_harris_priester(end,7);       % radiant
-        result_harris_priester = orbit_integral(a,e,I,Omega,...
+        a = result_msis(end,2);      % meter
+        e = result_msis(end,3);      % radiant
+        I = result_msis(end,4);
+        Omega = result_msis(end,5);   % radiant
+        w = result_msis(end,6);       % radiant
+        M = result_msis(end,7);       % radiant
+        result_msis = orbit_integral(a,e,I,Omega,...
             w,M,AdM,t0,tspan_split,"hp");
     end
-    name1 = ['orbit_data/result_noforce_',num2str(i),'.mat'];
-    name2 = ['orbit_data/result_hp_',num2str(i),'.mat'];
-    save(name1,'result_noforce')
-    save(name2,'result_harris_priester')
-    fprintf("%d day done",i)
+%     name1 = ['orbit_data/result_noforce_',num2str(i),'.mat'];
+%     save(name1,'result_noforce')
+%     name2 = ['orbit_data/result_hp_',num2str(i),'.mat'];
+%     save(name2,'result_harris_priester')
+    name3 = ['orbit_data/result_msis_',num2str(i),'.mat'];
+    save(name3,'result_msis')
+    fprintf("%d day done\n",i)
 end
 
--- Project_Space_Debris/Ordner_Yu/main_plot.m
+++ Project_Space_Debris/Ordner_Yu/main_plot.m
@@ -12,6 +12,10 @@ e_noforce = [];
 t_hp = [];
 a_hp = [];
 e_hp = [];
+
+t_msis = [];
+a_msis = [];
+e_msis = [];
 for i=1:length(files)
     if files(i).isdir
         continue
@@ -26,6 +30,11 @@ for i=1:length(files)
             t_hp = [t_hp;result_harris_priester(:,1)];
             a_hp = [a_hp;result_harris_priester(:,2)];
             e_hp = [e_hp;result_harris_priester(:,3)];
+        elseif files(i).name(8:11) == "msis"
+            load([files(i).folder,'/',files(i).name])
+            t_msis = [t_msis;result_msis(:,1)];
+            a_msis = [a_msis;result_msis(:,2)];
+            e_msis = [e_msis;result_msis(:,3)];
         end
     end
 end
@@ -39,21 +48,26 @@ e_noforce_sorted = e_noforce(I);
 a_hp_sorted = a_hp(I);
 e_hp_sorted = e_hp(I);
 
+[t_msis_sorted,I] = sort(t_msis);
+a_msis_sorted = a_msis(I);
+e_msis_sorted = e_msis(I);
 %%
 figure
+subplot(2,1,1)
 hold on
-plot(t_noforce_sorted/3600/24,(a_noforce_sorted-6378137)/1000)
-plot(t_hp_sorted/3600/24,(a_hp_sorted-6378137)/1000)
+plot(t_noforce_sorted(t_noforce_sorted<24*3600)/3600,(a_noforce_sorted(t_noforce_sorted<24*3600)-6378137)/1000)
+plot(t_hp_sorted(t_hp_sorted<24*3600)/3600,(a_hp_sorted(t_hp_sorted<24*3600)-6378137)/1000)
+plot(t_msis_sorted/3600,(a_msis_sorted-6378137)/1000)
 set(gca,'FontSize',20)
-xlabel("time (month)")
+xlabel("time (hour)")
 ylabel("a - R (km)")
-pbaspect([3,1,1])
 
-figure
+subplot(2,1,2)
 hold on
-plot(t_noforce_sorted/3600/24,e_noforce_sorted)
-plot(t_hp_sorted/3600/24,e_hp_sorted)
+plot(t_noforce_sorted(t_noforce_sorted<24*3600)/3600,e_noforce_sorted(t_noforce_sorted<24*3600))
+plot(t_hp_sorted(t_hp_sorted<24*3600)/3600,e_hp_sorted(t_hp_sorted<24*3600))
+plot(t_msis_sorted/3600,e_msis_sorted)
 set(gca,'FontSize',20)
-xlabel("time (month)")
+xlabel("time (hour)")
 ylabel("e")
-pbaspect([3,1,1])
+suptitle("sphere 4.84 m^2, 1250 kg")
