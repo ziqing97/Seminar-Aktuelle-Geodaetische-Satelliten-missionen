f845aa07d8f0cbe3c71aa988b1384488975e5905
ZIQING YU <ziqing.yu0623@gmail.com>
2023-01-31 13:25:26 +0100

    update integral function

--- Project_Space_Debris/Ordner_Yu/main.m
+++ Project_Space_Debris/Ordner_Yu/main.m
@@ -1,88 +1,14 @@
-%% init
+%
 clc;close all;clearvars
 
-%%
-R = 6378137;            % meter
-h = 811.5e3;              % meter
-mass = 1250;             % kg
-area = 2.2^2;              % m^2
-AdM = area/mass;
-
-% To Kepler Elements
-a = R + h;                   % meter
-I = deg2rad(98.622);         % radiant
-e = 0.0002125;
-Omega = deg2rad(335);   % radiant
-w = deg2rad(273);       % radiant
-M = deg2rad(5);         % radiant
-
-GM = 3.9865005e14;              % m^3/s^2
-T = sqrt((a^3 * 4* pi) / (GM)); % s
-
-mass = 140;
-area = 25;
-AdM = area/mass;
-I = 0;
-e = 0;
-a = 6378137+450e3;
-
-%%
-starttime = datetime(2022,12,1,0,0,0,'TimeZone','UTC');
-tspan = 0:24*3600:24*3600*365*2; % integral time
-
-tspan = 0:24*3600:16*24*3600;
-
-for i=1:length(tspan)-1
-    tspan_split=[tspan(i),tspan(i+1)];
-    t0 = starttime+tspan(i)/3600/24;
-    if i==1
-%         result_noforce = orbit_integral(a,e,I,Omega,...
-%             w,M,AdM,t0,tspan_split,"noforce");
-
-        result_harris_priester = orbit_integral(a,e,I,Omega,...
-            w,M,AdM,t0,tspan_split,"hp");
-
-%         result_msis = orbit_integral(a,e,I,Omega,...
-%             w,M,AdM,t0,tspan_split,"msis");
-    else
-%         % noforce
-%         a = result_noforce(end,2);              % meter
-%         e = result_noforce(end,3);      % radiant
-%         I = result_noforce(end,4);
-%         Omega = result_noforce(end,5);   % radiant
-%         w = result_noforce(end,6);       % radiant
-%         M = result_noforce(end,7);       % radiant
-%         result_noforce = orbit_integral(a,e,I,Omega,...
-%             w,M,AdM,t0,tspan_split,"noforce");
-% 
-        % harris-priester model
-        a = result_harris_priester(end,2);      % meter
-        e = result_harris_priester(end,3);      % radiant
-        I = result_harris_priester(end,4);
-        Omega = result_harris_priester(end,5);   % radiant
-        w = result_harris_priester(end,6);       % radiant
-        M = result_harris_priester(end,7);       % radiant
-        result_harris_priester = orbit_integral(a,e,I,Omega,...
-            w,M,AdM,t0,tspan_split,"hp");
-
-%         % msis model
-%         a = result_msis(end,2);      % meter
-%         e = result_msis(end,3);      % radiant
-%         I = result_msis(end,4);
-%         Omega = result_msis(end,5);   % radiant
-%         w = result_msis(end,6);       % radiant
-%         M = result_msis(end,7);       % radiant
-%         result_msis = orbit_integral(a,e,I,Omega,...
-%             w,M,AdM,t0,tspan_split,"msis");
+%
+R = 6378137;
+h = 600e3;
+a = R+h;
+for e = 0:0.01:0.05
+    for AdM = 0.005:0.01:0.075
+        object_orbit_integral(e,AdM,a)
     end
-%     name1 = ['orbit_data/result_noforce_',num2str(i),'.mat'];
-%     save(name1,'result_noforce')
-
-    name2 = ['orbit_data/result_hp_',num2str(i),'.mat'];
-    save(name2,'result_harris_priester')
-
-%     name3 = ['orbit_data/result_msis_',num2str(i),'.mat'];
-%     save(name3,'result_msis')
-    fprintf("%d day done\n",i)
 end
+
 
--- Project_Space_Debris/Ordner_Yu/object_orbit_integral.m
+++ Project_Space_Debris/Ordner_Yu/object_orbit_integral.m
@@ -1,36 +1,24 @@
-%% init
-clc;close all;clearvars
+function [] = object_orbit_integral(e,AdM,a)
 
-%%
-R = 6378137;            % meter
-h = 1000e3;              % meter
-mass = 1250;             % kg
-area = 5;              % m^2
-AdM = area/mass;
-
-% To Kepler Elements
-a = R + h;                   % meter
 I = deg2rad(98.622);         % radiant
-e = 0.1;
 Omega = deg2rad(335);   % radiant
 w = deg2rad(273);       % radiant
 M = deg2rad(5);         % radiant
 
-GM = 3.9865005e14;              % m^3/s^2
-T = sqrt((a^3 * 4* pi) / (GM)); % s
 
 %%
 starttime = datetime(2023,1,1,0,0,0,'TimeZone','UTC');
-tspan = 0:24*3600:24*3600*365*30; % integral time
-figure
-hold on
+tspan = 0:24*3600:24*3600*365*40; % integral time
+folder = ['C:/orbit/orbit_e_',num2str(e),'_AdM_',num2str(AdM)];
+if ~exist(folder,'dir')
+    mkdir(folder)
+end
 for i=1:length(tspan)-1
     tspan_split=[tspan(i),tspan(i+1)];
     t0 = starttime+tspan(i)/3600/24;
     if i==1
     result_harris_priester = orbit_integral(a,e,I,Omega,...
         w,M,AdM,t0,tspan_split,"hp");
-    plot(result_harris_priester(:,1),result_harris_priester(:,2)-6378137)
     else
     % harris-priester model
     a = result_harris_priester(end,2);      % meter
@@ -39,16 +27,15 @@ for i=1:length(tspan)-1
     Omega = result_harris_priester(end,5);   % radiant
     w = result_harris_priester(end,6);       % radiant
     M = result_harris_priester(end,7);       % radiant
-    if (a-a*e)<6378137+250e3
+    if (a + a * e) < 6378137 + 150e3
         break
     end
     result_harris_priester = orbit_integral(a,e,I,Omega,...
         w,M,AdM,t0,tspan_split,"hp");
-    plot(result_harris_priester(:,1),result_harris_priester(:,2)-6378137)
     end
-    name2 = ['E:/orbit/result_hp_',num2str(i),'.mat'];
-    save(name2,'result_harris_priester')
+    name = [folder,'/result_hp_',num2str(i),'.mat'];
+    save(name,'result_harris_priester')
+end
+disp(folder)
 
 end
-
-
